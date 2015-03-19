149f1a2a88db5cae98cdf36fef2ab3c0265a3c99
Dynse Clyde Sacote <beetlemon.cwts@gmail.com>
2015-03-20 06:26:06 +0800

    Update buse66_blockio_keys.php

--- backend/buse66_blockio_keys.php
+++ backend/buse66_blockio_keys.php
@@ -1,7 +1,7 @@
 <?php
-	define('BITCOIN_API_KEY', '189d-6932-a3f8-14ee');
-	define('DOGECOIN_API_KEY', '8697-31a0-479f-484e');
-	define('LITECOIN_API_KEY', '6b60-d465-7e0a-cc2f');
+	define('BITCOIN_API_KEY', '');
+	define('DOGECOIN_API_KEY', '');
+	define('LITECOIN_API_KEY', '');
 	define('VERSION', 2);
-	define('BLOCK_IO_PIN', 'desperaux1216');
+	define('BLOCK_IO_PIN', '');
 ?>
